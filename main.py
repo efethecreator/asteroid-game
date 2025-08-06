7d0909cde48eb15070b0bf4e385837b0604d0fde
murcetin <efe214360@hotmail.com>
2025-08-06 16:01:59 +0300

    asteroids were created and collisions were arranged

--- main.py
+++ main.py
@@ -1,45 +1,52 @@
 import pygame
 from constants import *
-from player import *
+from player import Player
+from asteroidfield import *
+
 
 def main():
     pygame.init()
-
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+    field = AsteroidField()
+
+
+    Player.containers = (updatable, drawable)    
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     dt = 0
 
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
-    x = SCREEN_WIDTH / 2
-    y = SCREEN_HEIGHT / 2
-
-    player = Player(x, y)
-
-    
-
     while True:
-        # Olayları kontrol et
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
-        # FPS sınırlayıcı ve zaman ölçüm
-        dt = clock.tick(60) / 1000
+        updatable.update(dt)
 
-        # Oyuncuyu güncelle (input'a göre döndür)
-        player.update(dt)
+        for asteroid in asteroids:
+            if player.check_collision(asteroid):
+                print("Game over!")
+                exit()
 
-        # Ekranı temizle
-        screen.fill((0, 0, 0))
 
-        # Oyuncuyu çiz
-        player.draw(screen)
+        screen.fill("black")
 
-        # Ekranı güncelle
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
 
-        
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
     main()
